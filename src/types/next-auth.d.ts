87456cc255ce9dcfe0b5293d92eb6cee6e20d39f
ALNO <thekurinji@gmail.com>
2026-07-14 17:42:50 +0700

    Update next-auth.d.ts

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,9 +1,11 @@
 import { DefaultSession } from "next-auth";
+import type { Role } from "@prisma/client";
 
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
+      role: Role;
     } & DefaultSession["user"];
   }
 }
